f93bdbb38ee9f8fb5a59d3e0032be3675bc2efe6
Eva-ja <eva_1999@tut.by>
2021-04-12 16:06:57 +0300

    create slider-review+ newsletter+fixed hotel-slide

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.hotel-slider', {
   
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--prev',
-    prevEl: '.slider-button--next',
+    nextEl: '.hotel-slider__button--prev',
+    prevEl: '.hotel-slider__button--next',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   keyboard: {
     enabled: true,
