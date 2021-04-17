8945b7a23b0d55021fc65a8fa997a90b0faa4cf5
Eva-ja <eva_1999@tut.by>
2021-04-17 17:42:52 +0300

    create esc-close,google map, adaptive for modal

--- js/main.js
+++ js/main.js
@@ -66,19 +66,13 @@ function closeModal(event) {
   modalDialog.removeClass("modal__dialog--visible");
 }
 
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [7.890703, 98.294772],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 15
-        });
+$(document).keyup(function(e) {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+    if (e.keyCode === 27) { 
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
     }
+});
 
 });
